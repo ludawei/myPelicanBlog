15f38f56f646441089bfbe547434301e46cd5d80
卢大维 <ludawei@tianqi.cn>
2019-01-30 15:19:50 +0800

    add post

--- make_html_pre.sh
+++ make_html_pre.sh
@@ -11,6 +11,6 @@ echo "同步文件到139.199.23.228"
 sshpass -p 'LvGWLru4RwtNGSzw2zQl' rsync -avz --exclude "*.git" --exclude "*.DS_Store" /Users/ludawei/important/ludawei_blog_github/* dav@139.199.23.228:/home/dav/www/blog/
 
 # echo "开启服务，本地预览：http://localhost:8000/"
-open http://blog.davhui.top/
+open http://139.199.23.228:50000
 
 # make serve
